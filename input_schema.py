e7b8cdb18640856a9fe0b3432872169ca44eedd0
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-05-02 13:15:31 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -3,6 +3,6 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["Hello, my name is Suno. And, uh — and I like pizza. [laughs] But I also have other interests such as playing tic tac toe."]
+        'example': ["What is AI?"]
     }
 }
